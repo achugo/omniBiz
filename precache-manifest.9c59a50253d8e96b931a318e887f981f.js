32f7ae82656df0d9c34778d542c36217f831307e
achugo <achugo2017@gmail>
2020-02-10 17:45:30 +0100

    Updates

---
+++ precache-manifest.9c59a50253d8e96b931a318e887f981f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891cf91fbac5de666ad7114dff4b5fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec77336c86917e3a9512",
+    "url": "/static/css/main.b71f5025.chunk.css"
+  },
+  {
+    "revision": "d1ff1c9b88a81d84a6fa",
+    "url": "/static/js/2.94e0972f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.94e0972f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec77336c86917e3a9512",
+    "url": "/static/js/main.bd6f17d6.chunk.js"
+  },
+  {
+    "revision": "579d3002e7351e73ecb5",
+    "url": "/static/js/runtime-main.53b58625.js"
+  }
+]);
